07c9d4c20e10ee71a54cf1cd3a2277c17bf718a2
Bill Delvin <billdelvin6@gmail.com>
2020-08-23 18:07:17 +0700

    adding middleware

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -4,7 +4,9 @@ const privateKey = "privateKey";
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, privateKey);
+    const decodeToken = jwt.verify(token, privateKey);
+    // in here create new req that is userData
+    req.userData = { email: decodeToken.email, userId: decodeToken.userId };
     next();
   } catch (error) {
     res.status(401).json({ message: "Auth Failed" });
